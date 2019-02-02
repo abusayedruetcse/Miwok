826f5aa018f7d211aaa9cf589cf9e458018624e5
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-02-02 14:14:26 +0600

    getView redefined for WordAdapter

--- app/src/main/java/com/ecxample/android/miwok/WordAdapter.java
+++ app/src/main/java/com/ecxample/android/miwok/WordAdapter.java
@@ -1,12 +1,52 @@
 package com.ecxample.android.miwok;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word> banglaWord)
+    {
+        super(context,0,banglaWord);
+
+    }
     @Override
     public View getView(int position,View convertView,ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Word currentAndroidFlavor = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID version_name
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.bangla);
+        // Get the version name from the current AndroidFlavor object and
+        // set this text on the name TextView
+        nameTextView.setText(currentAndroidFlavor.getBanglaTranslation());
+
+        // Find the TextView in the list_item.xml layout with the ID version_number
+        TextView numberTextView = (TextView) listItemView.findViewById(R.id.english);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
+        numberTextView.setText(currentAndroidFlavor.getDefaultTranslation());
+
+
+
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
     }
 }
