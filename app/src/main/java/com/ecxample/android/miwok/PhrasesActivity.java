e591d87e804606d8d0cf1f7121874a582aaa5da0
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-02-03 00:27:29 +0600

    PhrasesActivity .java & .xml & list_phrases.xml added

--- app/src/main/java/com/ecxample/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/ecxample/android/miwok/PhrasesActivity.java
@@ -2,6 +2,9 @@ package com.ecxample.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ArrayList<Word> phrases=new ArrayList<Word>();
+        phrases.add(new Word("Name","Abu Abdullah"));
+        phrases.add(new Word("Country","Bangladesh"));
+        phrases.add(new Word("Favourite Fruit","Mango"));
+
+        WordAdapter wordAdapter=new WordAdapter(this,phrases);
+        ListView listView=(ListView)findViewById(R.id.phrases);
+        listView.setAdapter(wordAdapter);
+
     }
 }
